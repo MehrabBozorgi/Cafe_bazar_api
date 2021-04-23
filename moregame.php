858cccc22330649f15871a6ea67789f7c1fbb8a2
MehrabBozorgi <mehrab.bozorgi@gmail.com>
2021-04-23 14:07:13 +0430

    api

---
+++ moregame.php
@@ -0,0 +1,29 @@
+<?php
+include "connect.php";
+$apps = array();
+$query = "SELECT * FROM game ";
+$res = $connection->prepare($query);
+$res->execute();
+while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+    $record = array();
+    $record["id"] = $row["id"];
+    $record["name"] = $row["name"];
+    $record["description"] = $row["description"];
+    $record["kind"] = $row["kind"];
+    $record["cat"] = $row["cat"];
+    $record["cat_id"] = $row["cat_id"];
+    $record["download"] = $row["download"];
+    $record["size"] = $row["size"];
+    $record["img_slide"] = $row["img_slide"];
+    $record["version"] = $row["version"];
+    $record["icon"] = $row["icon"];
+    $record["developer_phone"] = $row["developer_phone"];
+    $record["developer_name"] = $row["developer_name"];
+    $record["developer_email"] = $row["developer_email"];
+    $record["developer_web"] = $row["developer_web"];
+
+    $apps[] = $record;
+}
+shuffle($apps);
+
+echo JSON_encode($apps);
